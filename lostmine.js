9a99af352de4640bd3b42917c43ebecbb6842dd5
Jeff Sprague <spragujr@cisco.com>
2015-10-17 16:28:56 -0700

    Added dynamite drop and digging trails.

--- lostmine.js
+++ lostmine.js
@@ -12,13 +12,48 @@ var foreLayerContext;
 var walking = false;
 var minerX = 320;
 var minerY = 40;
+var minerCol = parseInt(minerX / 32);
+var minerRow = parseInt((minerY - 40) / 32);
 var moveX = 0;
 var moveY = 0;
 var pendingX = 0;
 var pendingY = 0;
 var moveCounter = 31;
-var map = [];
-var tiles = [];
+
+// Miner Frame Numbers
+var STAND_DOWN = 0;
+var WALK_DOWN_START = 1;
+var WALK_DOWN_END = 2;
+var STAND_UP = 3;
+var WALK_UP_START = 4;
+var WALK_UP_END = 5;
+var STAND_LEFT = 6;
+var WALK_LEFT_START = 6;
+var WALK_LEFT_END = 7;
+var STAND_RIGHT = 8;
+var WALK_RIGHT_START = 8;
+var WALK_RIGHT_END = 9;
+var DEAD_LEFT = 10;
+var DEAD_RIGHT = 11;
+
+var dynamiteDropped = false;
+var dynamiteSprite;
+
+// Tile Map definitions
+var NOTHING = -1;
+var SURFACE = 0;
+var DIRT = 1;
+var ROCK = 2;
+var SILVER_LG = 3;
+var SILVER_SM = 4;
+var ROCKS = 5;
+var GOLD_MD = 6;
+var GOLD_SM = 7;
+var GOLD_LG = 8;
+var DUGOUT = 9;
+
+var tileMap = [];
+var tileCoords = [{x:96, y:128},{x:0, y:96},{x:32, y:96},{x:64, y:96},{x:96, y:96},{x:128, y:96},{x:0, y:128},{x:32, y:128},{x:64, y:128}];
 
 //***********************************************
 var init = function () {
@@ -35,48 +70,45 @@ var init = function () {
     document.addEventListener("keyup", processKeyUp);
 
     function processKeyDown(ev) {
-        console.log("KEY: "+ev.keyCode);
-        var frame;
+//        console.log("KEY: "+ev.keyCode);
+        var tile;
 
         keyState[ev.keyCode] = true;
         switch (ev.keyCode) {
-            case 88:
-            {
-                //explSprite.animate = true;
-                explSprite.setAnimActions(true);
-                explSprite.setPosition(jgl.random(500) + 100, jgl.random(200) + 100);
-                explSprite.setCurrentFrame(0);
-                explSprite.show();
-                break;
-            }
-
-            case 39:    // RIGHT
-                walking = true;
-                pendingX = 1;
-                pendingY = 0;
+            case 88:    // X - dynamite
+                dropDynamite(minerRow, minerCol);
                 break;
 
-            case 37:    // LEFT
-                walking = true;
-                pendingX = -1;
-                pendingY = 0;
+            case KEYS.RIGHT:
+                if (canWalk('RIGHT', minerRow, minerCol + 1)) {
+                    walking = true;
+                    pendingX = 1;
+                    pendingY = 0;
+                }
                 break;
 
-            case 38:    // UP
-                walking = true;
-                pendingX = 0;
-                pendingY = -1;
+            case KEYS.LEFT:
+                if (canWalk('LEFT', minerRow, minerCol - 1)) {
+                    walking = true;
+                    pendingX = -1;
+                    pendingY = 0;
+                }
                 break;
 
-            case 40:    // DOWN
-                walking = true;
-                pendingX = 0;
-                pendingY = 1;
+            case KEYS.UP:
+                if (canWalk('UP', minerRow - 1, minerCol)) {
+                    walking = true;
+                    pendingX = 0;
+                    pendingY = -1;
+                }
                 break;
 
-            case  83:   // S
-                pendingX = 0;
-                pendingY = 0;
+            case KEYS.DOWN:
+                if (canWalk('DOWN', minerRow + 1, minerCol)) {
+                    walking = true;
+                    pendingX = 0;
+                    pendingY = 1;
+                }
                 break;
         }
     }
@@ -88,9 +120,6 @@ var init = function () {
         }
     }
 
-    initSprites();
-
-    animate();
 };
 
 //***********************************************
@@ -111,22 +140,6 @@ initSprites = function() {
         active: true
     });
 
-    // Frame Numbers
-    STAND_DOWN = 0;
-    WALK_DOWN_START = 1;
-    WALK_DOWN_END = 2;
-    STAND_UP = 3;
-    WALK_UP_START = 4;
-    WALK_UP_END = 5;
-    STAND_LEFT = 6;
-    WALK_LEFT_START = 6;
-    WALK_LEFT_END = 7;
-    STAND_RIGHT = 8;
-    WALK_RIGHT_START = 8;
-    WALK_RIGHT_END = 9;
-    DEAD_LEFT = 10;
-    DEAD_RIGHT = 11;
-
     miner.setAnimFrame(STAND_DOWN, imageMap, 0, 0, 32, 32);    // Standing still
     miner.setAnimFrame(WALK_DOWN_START, imageMap, 0, 32, 32, 32);    // walking
     miner.setAnimFrame(WALK_DOWN_END, imageMap, 0, 64, 32, 32);    // walking
@@ -139,6 +152,13 @@ initSprites = function() {
     miner.setAnimFrame(WALK_RIGHT_END, imageMap, 96, 32, 32, 32);    // walking right
     miner.setAnimFrame(DEAD_LEFT, imageMap, 128, 32, 32, 32);    // dead left
     miner.setAnimFrame(DEAD_RIGHT, imageMap, 128, 0, 32, 32);    // dead right
+
+    // DYNAMITE sprite
+    dynamiteSprite = spriteList.newSprite({id: 'dynamite',
+        width: 32, height: 32,
+        active: false
+    });
+    dynamiteSprite.setImage(imageMap, 128, 128, 32, 32);
 
     // EXPLOSION sprite
     explSprite = spriteList.newSprite({
@@ -159,11 +179,12 @@ initSprites = function() {
         explSprite.setAnimFrame(frame, explosionImg, frame * 88, 0, 88, 90);
     }
     explSprite.setHotSpot(44, 44);
+
 };
 
 //***********************************************
 var initLayers = function() {
-    var i, x, y;
+    var i, x, y, t, tc;
 
     explosionImg = jgl.newImage("./images/explosion.png");
 
@@ -176,48 +197,10 @@ var initLayers = function() {
     });
 
     imageMap = jgl.newImage("./images/miner.png", function() {
-        var SURFACE = 0;
-        var DIRT = 1;
-        var ROCK = 2;
-        var SILVER_LG = 3;
-        var SILVER_SM = 4;
-        var ROCKS = 5;
-        var GOLD_MD = 6;
-        var GOLD_SM = 7;
-        var GOLD_LG = 8;
-
-        tiles = [{x:96, y:128},{x:0, y:96},{x:32, y:96},{x:64, y:96},{x:96, y:96},{x:128, y:96},{x:0, y:128},{x:32, y:128},{x:64, y:128}];
-        map.push([SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE]);
-        for (i = 0; i < 8; i++) {
-            var row = [DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT];
-            map.push(row);
-        }
-        // Scatter treasure
-        var treasures = jgl.random(20) + 1;
-        for (i = 0; i < treasures; i++) {
-            var x = jgl.random(20);
-            var y = jgl.random(8) + 1;
-            map[y][x] = jgl.random(6) + 3;
-        }
-        // Scatter rocks
-        for (i = 0; i < 25; i++) {
-            var x = jgl.random(20);
-            var y = jgl.random(8) + 1;
-            map[y][x] = ROCKS;
-        }
-        for (i = 0; i < 25; i++) {
-            var x = jgl.random(20);
-            var y = jgl.random(8) + 1;
-            map[y][x] = ROCK;
-        }
-        // Draw the map
-        for (x = 0; x < 20; x++) {
-            for (y = 0; y < 9; y++) {
-                var t = map[y][x];
-                var tc = tiles[t];
-                backLayerContext.drawImage(imageMap, tc.x, tc.y, 32, 32, x*32, 72+(y*32), 32, 32);
-            }
-        }
+        initSprites();
+        populateMine();
+        drawMine();
+        animate();
 
     });
 
@@ -237,6 +220,86 @@ var initLayers = function() {
 };
 
 //***********************************************
+var populateMine = function() {
+    var i, x, y;
+
+    tileMap.push([  NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,
+        NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING,NOTHING]);
+    tileMap.push([  SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,
+                SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE,SURFACE]);
+
+    // Fill the tileMap with plain dirt
+    for (i = 0; i < 8; i++) {
+        var row = [DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT,DIRT];
+        tileMap.push(row);
+    }
+    // Scatter treasure
+    var treasures = jgl.random(20) + 1;
+    for (i = 0; i < treasures; i++) {
+        x = jgl.random(20);
+        y = jgl.random(8) + 2;
+        tileMap[y][x] = jgl.random(6) + 3;
+    }
+    // Scatter rocks
+    for (i = 0; i < 25; i++) {
+        x = jgl.random(20);
+        y = jgl.random(8) + 2;
+        tileMap[y][x] = ROCKS;
+    }
+    for (i = 0; i < 25; i++) {
+        x = jgl.random(20);
+        y = jgl.random(8) + 2;
+        tileMap[y][x] = ROCK;
+    }
+};
+
+//***********************************************
+var drawMine = function() {
+    var row, col, t, tc;
+    for (col = 0; col < 20; col++) {
+        for (row = 0; row < 10; row++) {
+            t = tileMap[row][col];
+            if (t !== NOTHING && t !== DUGOUT) {
+                tc = tileCoords[t];
+                backLayerContext.drawImage(imageMap, tc.x, tc.y, 32, 32, col*32, 40+(row*32), 32, 32);
+            }
+        }
+    }
+};
+
+//***********************************************
+var canWalk = function(dir, row, col) {
+    if (row < 0 || row > 8) { return false; }       // off-screen
+    if (col < 0 || col > 19) { return false; }      // off-screen
+
+    var t = tileMap[row][col];
+    console.log("TILE @ "+row+","+col+" = " + t);
+    if (t === 2 || t === 5) { return false; }       // Rocks
+    return true;
+};
+
+//***********************************************
+var dropDynamite = function(row, col) {
+    if (!dynamiteDropped) {
+        var x = col * 32;
+        var y = 40 + row * 32;
+        dynamiteSprite.setPosition(x, y);
+        dynamiteSprite.show();
+        setTimeout(function() { detonateDynamite(x + 16, y + 16); }, 2000);
+    }
+};
+
+//***********************************************
+var detonateDynamite = function(x, y) {
+    dynamiteDropped = false;
+    dynamiteSprite.hide();
+    explSprite.setAnimActions(true);
+    explSprite.setPosition(x, y);
+    explSprite.setCurrentFrame(0);
+    explSprite.show();
+};
+
+//***********************************************
 var animate = function(){
     // request new frame
     requestAnimFrame(function(){ animate(); });
@@ -252,6 +315,7 @@ var updateMiner = function(){
             moveCounter = 0;
             moveX = pendingX;
             moveY = pendingY;
+            pendingX = pendingY = 0;
 
             if (moveX > 0) {
                 miner.setFrameRange(WALK_RIGHT_START, WALK_RIGHT_END, WALK_RIGHT_END);
@@ -289,6 +353,12 @@ var updateMiner = function(){
         }
 
         miner.setPosition(minerX, minerY);
+        if (minerY > 71) {
+            backLayerContext.beginPath();
+            backLayerContext.arc(minerX + 16,minerY + 16,16,0,2*Math.PI);
+            backLayerContext.fillStyle = "#200";
+            backLayerContext.fill();
+        }
     }
 };
 
@@ -302,6 +372,10 @@ var stopWalking = function() {
         if (moveX < 0) { frame = STAND_LEFT; }
         if (moveY > 0) { frame = STAND_DOWN; }
         if (moveY < 0) { frame = STAND_UP; }
+        minerCol = parseInt(minerX / 32);
+        minerRow = parseInt((minerY - 40) / 32);
+        tileMap[minerRow][minerCol] = DUGOUT;
+        console.log("MINER: "+minerRow+','+minerCol);
         miner.setAnimActions(false);
         miner.setCurrentFrame(frame);
     }
